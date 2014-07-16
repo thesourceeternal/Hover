3c94f5f2cd9e700a819d82d7ab27df7a3d02e1c6
knod <knodding@gmail.com>
2014-07-16 09:12:11 -0400

    Gets rid of redundant index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+// For now this starts things up
+// Perhaps later it will be in a game.js
+
+window.addEventListener( 'load', function () {
+
+	// Make all the pointer lock things work
+	pointerLock._init_();
+
+	// Add event listeners
+	mouseEvents();
+	keyEvents();
+
+} );  // end window on load
+
