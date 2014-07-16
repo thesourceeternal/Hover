baff138155d817dd7920583fe09024b9bb1b18ae
knod <knodding@gmail.com>
2014-07-16 15:06:48 -0400

    Different html for tabs for js functionality, css margin & padding

--- js/display/htmlBlocks.js
+++ js/display/htmlBlocks.js
@@ -4,12 +4,14 @@
 
 var htmlBlocks = {
 
-	editorTabs: "<div class='spacer editors-tab-spacer'></div>\n"+
+	editorTabs: "<div class='tab-container'>  <!-- in here for js -->\n"+
+	"<div class='spacer editors-tab-spacer'></div>\n"+
     "<div class='faux-elem'>Inspector</div>\n"+
-    "<nav id='sidebar-nav' class='editors-tabs editor-menu'>\n"+
+    "<menu id='sidebar-nav' class='editors-tabs editor-menu'>\n"+
     "    <a class='tab left-tab inspector-tab active-tab'>Inspector</a>\n"+
     "    <a class='tab right-tab assets-tab'>Assets</a>\n"+
-    "</nav>",
+    "</menu>\n"+
+    "</div>",
 
 	sceneTree: null, 
 
